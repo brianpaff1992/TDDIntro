580ba6f43f09a244b04473ed936db52bb44c95c0
brianpaff1992 <brianpaff1992@gmail.com>
2015-09-01 17:10:19 -0700

    FactorialTest Test 3 Passed

--- src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
@@ -3,7 +3,7 @@ package com.thoughtworks.tddintro.factorial;
 public class Factorial {
     public Integer compute(int i) {
 
-        if(i == 1)
+        if(i <= 1)
         {
             return 1;
         }
