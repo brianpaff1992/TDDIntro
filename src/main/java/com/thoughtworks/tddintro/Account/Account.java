1de10b3d7791ebc13ff2a1e7d2b983bb5f5d6d97
brianpaff1992 <brianpaff1992@gmail.com>
2015-09-01 17:31:53 -0700

    AccountTest FrameWork Finished
    Created Account Class, uncommented AccountTest, created @Ignores for
    later tests

---
+++ src/main/java/com/thoughtworks/tddintro/Account/Account.java
@@ -0,0 +1,18 @@
+package com.thoughtworks.tddintro.Account;
+
+/**
+ * Created by Brian on 9/1/2015.
+ */
+public class Account {
+    private int amount;
+
+    public Account()
+    {
+        amount = 0;
+    }
+
+    public void deposit(int i)
+    {
+
+    }
+}
--- src/test/java/com/thoughtworks/tddintro/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/tddintro/accountbalance/AccountTests.java
@@ -1,18 +1,24 @@
 package com.thoughtworks.tddintro.accountbalance;
 
+import org.junit.Ignore;
+import org.junit.Test;
+
 public class AccountTests {
-//    @Test
-//    public void shouldIncreaseMyBalanceWhenIDepositMoney(){
-//
-//    }
-//
-//    @Test
-//    public void shouldDecreaseMyBalanceWhenIWithdrawMoney(){
-//
-//    }
-//
-//    @Test
-//    public void shouldNotDecreaseMyBalanceWhenIWithdrawMoneyAndDoNotHaveEnoughToCoverTheWithdrawal(){
-//
-//    }
+
+        @Test
+        public void shouldIncreaseMyBalanceWhenIDepositMoney(){
+
+        }
+
+        @Test
+        @Ignore
+        public void shouldDecreaseMyBalanceWhenIWithdrawMoney(){
+
+        }
+
+        @Test
+        @Ignore
+        public void shouldNotDecreaseMyBalanceWhenIWithdrawMoneyAndDoNotHaveEnoughToCoverTheWithdrawal(){
+
+        }
 }
